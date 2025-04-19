29c78d3e326c7672f6e28d83597db4d4b8829253
Malottey1 <malcolm.clottey@ashesi.edu.gh>
2025-04-19 18:07:57 +0000

    update

--- libs/scheduling/setup.py
+++ libs/scheduling/setup.py
@@ -8,6 +8,8 @@ setup(
       "Flask>=2.0",                     # for the feasibility blueprint
       "mysql-connector-python>=8.0",    # for your DB calls
       "pygad>=2.16",                    # the GA you’re using
-      "pandas>=1.3",                    # for any in‑lib dataframe ops
+      "pandas>=1.3", 
+      "Jinja2>=3.0",
+      "SQLAlchemy>=1.4",                   
     ],
 )
