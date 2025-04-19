f29d99e2e583f1e453941a69f5bf72e3abcd3b72
Malottey1 <malcolm.clottey@ashesi.edu.gh>
2025-04-19 13:50:49 +0000

    setup.py change

--- libs/scheduling/setup.py
+++ libs/scheduling/setup.py
@@ -2,6 +2,6 @@ from setuptools import setup, find_packages
 
 setup(
     name="scheduling",
-    version="0.1",
-    packages=find_packages(),      
+    version="0.1.0",
+    packages=find_packages(),  # picks up scheduler.py, feasibility_checker.py, etc.
 )
